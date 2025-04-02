27f06fad1c124a594a3b9885fa5508ffb0f8fb4b
Maranguan <38739244+Maranguan@users.noreply.github.com>
2025-04-02 12:47:40 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<!-- includes/footer.php -->
-<footer class="footer bg-light mt-5">
-    <div class="container">
-        <span class="text-muted">© 2025 Gestor de Stock. Todos los derechos reservados.</span>
-    </div>
-</footer>
-
-</body>
-</html>
